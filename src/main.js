1de82517a7f05c2d77fbad333ff61b308179a373
Emeric Krause <ekrause@k0r1p11.42mulhouse.fr>
2025-12-16 11:08:33 +0100

    init board pieces and squares

--- src/main.js
+++ src/main.js
@@ -1,112 +1,57 @@
 var canvas = document.getElementById("gameCanvas");
 var ctx = canvas.getContext("2d");
-var keys = {
-    w: false,
-    a: false,
-    s: false,
-    d: false
-};
 //####################
 // Classes
-var Player = /** @class */ (function () {
-    function Player(x, y, width, height, speed) {
-        this.x = x;
-        this.y = y;
-        this.dx = 0;
-        this.dy = 0;
-        this.width = width;
-        this.height = height;
-        this.speed = speed;
+var Piece = /** @class */ (function () {
+    function Piece(t, c) {
+        this.type = t;
+        this.color = c;
     }
-    Player.prototype.draw = function () {
-        ctx === null || ctx === void 0 ? void 0 : ctx.save();
-        ctx.fillStyle = "white";
-        ctx === null || ctx === void 0 ? void 0 : ctx.fillRect(this.x, this.y, this.width, this.height);
-        ctx === null || ctx === void 0 ? void 0 : ctx.restore();
-    };
-    Player.prototype.update = function () {
-        var tempDx = 0;
-        var tempDy = 0;
-        if (keys.w)
-            tempDy -= this.speed;
-        if (keys.a)
-            tempDx -= this.speed;
-        if (keys.s)
-            tempDy += this.speed;
-        if (keys.d)
-            tempDx += this.speed;
-        this.dx = tempDx;
-        this.dy = tempDy;
-        this.x += this.dx;
-        this.y += this.dy;
-        if (this.x <= 0)
-            this.x = 0;
-        if (this.y <= 0)
-            this.y = 0;
-        if (this.x >= canvas.width - this.width)
-            this.x = canvas.width - this.width;
-        if (this.y >= canvas.height - this.height)
-            this.y = canvas.height - this.height;
-    };
-    return Player;
+    return Piece;
 }());
-// class Monster
-// {
-//     x:      number;
-//     y:      number;
-//     dx:     number;
-//     dy:     number;
-//     width:  number;
-//     height: number;
-//     speed:  number;
-//     constructor(x: number, y: number, width: number, height: number, speed: number) {
-//         this.x = x;
-//         this.y = y;
-//         this.dx = 0;
-//         this.dy = 0;
-//         this.width = width;
-//         this.height = height;
-//         this.speed = speed;
-//     }
-//     draw() {
-//         ctx?.save();
-//         ctx!.fillStyle = "red";
-//         ctx?.fillRect(this.x, this.y, this.width, this.height);
-//         ctx?.restore();
-//     }
-//     update(player: Player) {
-//         let dx = player.x - this.x;
-//         let dy = player.y - this.y;
-//         let distance = Math.sqrt(dx*dx + dy*dy);
-//         this.x += this.dx;
-//         this.y += this.dy;
-//         if (this.x <= 0)
-//             this.x = 0;
-//         if (this.y <= 0)
-//             this.y = 0;
-//         if (this.x >= canvas.width - this.width)
-//             this.x = canvas.width - this.width;
-//         if (this.y >= canvas.height - this.height)
-//             this.y = canvas.height - this.height;
-//     }
-// }
+var Square = /** @class */ (function () {
+    function Square(p, c) {
+        this.piece = p;
+        this.color = c;
+    }
+    return Square;
+}());
+var Board = /** @class */ (function () {
+    function Board(s) {
+        this.squares = [];
+        this.squareSize = s;
+    }
+    Board.prototype.initSquares = function () {
+        for (var y = 0; y < 8; y++) {
+            this.squares[y] = [];
+            for (var x = 0; x < 8; x++) {
+                var color = void 0;
+                if ((x + y) % 2 === 0)
+                    color = "white";
+                else
+                    color = "black";
+                this.squares[y][x] = new Square(null, color);
+            }
+        }
+    };
+    Board.prototype.drawSquares = function () {
+        for (var y = 0; y < 8; y++) {
+            for (var x = 0; x < 8; x++) {
+                ctx === null || ctx === void 0 ? void 0 : ctx.save();
+                ctx.fillStyle = this.squares[y][x].color;
+                ctx === null || ctx === void 0 ? void 0 : ctx.fillRect(x * this.squareSize + 2, y * this.squareSize + 2, this.squareSize, this.squareSize);
+                ctx === null || ctx === void 0 ? void 0 : ctx.restore();
+            }
+        }
+    };
+    return Board;
+}());
 //####################
-var player = new Player(canvas.width / 2, canvas.height / 2, 50, 50, 10);
-//const monster = new Monster(0, 0, 50, 50, 5);
-document.addEventListener("keydown", function (e) {
-    if (e.key in keys)
-        keys[e.key] = true;
-});
-document.addEventListener("keyup", function (e) {
-    if (e.key in keys)
-        keys[e.key] = false;
-});
+var board = new Board(32);
 function gameLoop() {
     ctx === null || ctx === void 0 ? void 0 : ctx.clearRect(0, 0, canvas.width, canvas.height);
-    player.update();
-    player.draw();
-    // monster.update();
-    // monster.draw();
-    requestAnimationFrame(gameLoop);
+    board.initSquares();
+    board.drawSquares();
+    //requestAnimationFrame(gameLoop);
 }
 gameLoop();
--- src/main.ts
+++ src/main.ts
@@ -1,139 +1,87 @@
 const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d");
 
-const keys = {
-    w: false,
-    a: false,
-    s: false,
-    d: false,
-};
-
 //####################
 // Classes
-class Player
-{
-    x:      number;
-    y:      number;
-    dx:     number;
-    dy:     number;
-    width:  number;
-    height: number;
-    speed:  number;
+class Piece{
+    type: string;
+    color: "white" | "black";
 
-    constructor(x: number, y: number, width: number, height: number, speed: number) {
-        this.x = x;
-        this.y = y;
-        this.dx = 0;
-        this.dy = 0;
-        this.width = width;
-        this.height = height;
-        this.speed = speed;
-    }
-
-    draw() {
-        ctx?.save();
-        ctx!.fillStyle = "white";
-        ctx?.fillRect(this.x, this.y, this.width, this.height);
-        ctx?.restore();
-    }
-
-    update() {
-        let tempDx = 0;
-        let tempDy = 0;
-
-        if (keys.w)
-            tempDy -= this.speed;
-        if (keys.a)
-            tempDx -= this.speed;
-        if (keys.s)
-            tempDy += this.speed;
-        if (keys.d)
-            tempDx += this.speed;
-
-        this.dx = tempDx;
-        this.dy = tempDy;
-
-        this.x += this.dx;
-        this.y += this.dy;
-
-        if (this.x <= 0)
-            this.x = 0;
-        if (this.y <= 0)
-            this.y = 0;
-        if (this.x >= canvas.width - this.width)
-            this.x = canvas.width - this.width;
-        if (this.y >= canvas.height - this.height)
-            this.y = canvas.height - this.height;
+    constructor(t: string, c: "white" | "black"){
+        this.type = t;
+        this.color = c
     }
 }
 
-// class Monster
-// {
-//     x:      number;
-//     y:      number;
-//     dx:     number;
-//     dy:     number;
-//     width:  number;
-//     height: number;
-//     speed:  number;
+class Square{
+    piece: Piece | null;
+    color: "white" | "black";
 
-//     constructor(x: number, y: number, width: number, height: number, speed: number) {
-//         this.x = x;
-//         this.y = y;
-//         this.dx = 0;
-//         this.dy = 0;
-//         this.width = width;
-//         this.height = height;
-//         this.speed = speed;
-//     }
+    constructor(p: Piece | null, c: "white" | "black"){
+        this.piece = p;
+        this.color = c;
+    }
+}
 
-//     draw() {
-//         ctx?.save();
-//         ctx!.fillStyle = "red";
-//         ctx?.fillRect(this.x, this.y, this.width, this.height);
-//         ctx?.restore();
-//     }
+class Board{
+    squares: Square[][] = [];
+    squareSize: number;
 
-//     update(player: Player) {
-//         let dx = player.x - this.x;
-//         let dy = player.y - this.y;
-//         let distance = Math.sqrt(dx*dx + dy*dy);
+    constructor(s: number){
+        this.squareSize = s;
+    }
+
+    initSquares(){
+        for (let y = 0; y < 8; y++){
+            this.squares[y] = [];
+            for (let x = 0; x < 8; x++){
+                let color: "white" | "black";
+                
+                if ((x + y) % 2 === 0)
+                    color = "white";
+                else
+                    color = "black";
+                this.squares[y][x] = new Square(null, color);
+            }
+        }
+    }
+
+    drawSquares(){
+        for(let y = 0; y < 8; y++){
+            for (let x = 0; x < 8; x++){
+                ctx?.save();
+                ctx!.fillStyle = this.squares[y][x].color;
+                ctx?.fillRect(x * this.squareSize + 2, y * this.squareSize + 2, this.squareSize, this.squareSize);
+                ctx?.restore();
+            }
+        }
+    }
+
+    initPieces(){
+        const backRowPieces = ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"]
+        for(let x = 0; x < 8; x++){
+            this.squares[0][x].piece = new Piece(backRowPieces[x], "black");
+            this.squares[1][x].piece = new Piece("pawn", "black");
+            this.squares[7][x].piece = new Piece(backRowPieces[x], "white");
+            this.squares[6][x].piece = new Piece("pawn", "white");
+        }
+    }
+
+    drawPieces(){
         
-//         this.x += this.dx;
-//         this.y += this.dy;
-//         if (this.x <= 0)
-//             this.x = 0;
-//         if (this.y <= 0)
-//             this.y = 0;
-//         if (this.x >= canvas.width - this.width)
-//             this.x = canvas.width - this.width;
-//         if (this.y >= canvas.height - this.height)
-//             this.y = canvas.height - this.height;
-//     }
-// }
+    }
+}
 //####################
 
-const player = new Player(canvas.width / 2, canvas.height / 2, 50, 50, 10);
-//const monster = new Monster(0, 0, 50, 50, 5);
-
-document.addEventListener("keydown", (e) => {
-    if (e.key in keys)
-        keys[e.key as keyof typeof keys] = true;
-})
-
-document.addEventListener("keyup", (e) => {
-    if (e.key in keys)
-        keys[e.key as keyof typeof keys] = false;
-})
+const board = new Board(32);
 
 function gameLoop() {
     ctx?.clearRect(0, 0, canvas.width, canvas.height);
-    player.update();
-    player.draw();
 
-    // monster.update();
-    // monster.draw();
-    requestAnimationFrame(gameLoop);
+    board.initSquares();
+    board.drawSquares();
+    //requestAnimationFrame(gameLoop);
 }
+
 
 gameLoop();
